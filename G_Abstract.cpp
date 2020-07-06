95a62d4b8dbbf384021f17944932e6c5b6cd3dd3
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-06 23:20:24 +0200

    Added Routines

--- G_Abstract.cpp
+++ G_Abstract.cpp
@@ -5,4 +5,21 @@ class Employee
 public:
     //public constructors and destructors
     Employee();
+    Employee(
+        FullName name,
+        String address,
+        String workPhone,
+        String homePhone,
+        TaxId taxIdNumber,
+        JobClassification jobClass
+
+    );
+    virtual ~Employee();
+    //public routines
+    FullName GetName() const;
+    String GetAddress() const;
+    String GetWorkPhone() const;
+    String GetHomePhone() const;
+    TaxId GetTaxIdNumber() const;
+    JobClassification GetJobClassification() const;
 };
