9eb50b1d39f1943ad5f4dc3cc4be15a09fa1b0cc
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-14 22:00:04 +0200

    Added MainMenu

--- guessMyNumber.cpp
+++ guessMyNumber.cpp
@@ -9,6 +9,24 @@ int upperBound = difficulty + 1; // Highest possible number left
 int myGuess = 0;
 int lastGuess = -1;
 
+bool MainMenu()
+{
+    cout << "Welcome to 'Guess my Number!'" << endl
+         << endl;
+    while (true)
+    {
+        cout << "1. Play" << endl;
+        cout << "2. Exit" << endl;
+        cout << ">";
+        cin >> input;
+
+        if (input == '1')
+            return true; // Starts the game.
+        else if (input == '2')
+            return false; // Quits the game
+    }
+}
+
 //Last section
 int main()
 {
