e3d41171f3bf8cca8c2c3a15ccb291a123d5fb04
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-14 21:52:07 +0200

    Updated

--- guessMyNumber.cpp
+++ guessMyNumber.cpp
@@ -3,3 +3,8 @@
 using namespace std;
 
 int difficulty = 10;
+char input = 'userNumber';
+int lowerBound = 0;              // Lowest possible number left
+int upperBound = difficulty + 1; // Highest possible number left
+int myGuess = 0;
+int lastGuess = -1;
