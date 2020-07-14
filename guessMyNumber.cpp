dac6223639235b4714660e748df69da238ce6b83
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-14 21:53:42 +0200

    Updated

--- guessMyNumber.cpp
+++ guessMyNumber.cpp
@@ -8,3 +8,14 @@ int lowerBound = 0;              // Lowest possible number left
 int upperBound = difficulty + 1; // Highest possible number left
 int myGuess = 0;
 int lastGuess = -1;
+
+//Last section
+int main()
+{
+    while (MainMenu())
+    { // While they say they want to play
+        while (PlayGame())
+            ; // repeateadly run the game loop
+    }
+    return 0;
+}
