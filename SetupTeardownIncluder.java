d35a5f4ed9085c74288c0d5bb9a7d57a1646b0eb
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-22 18:11:41 +0200

    Abstract, clean code

--- SetupTeardownIncluder.java
+++ SetupTeardownIncluder.java
@@ -43,4 +43,10 @@ public class SetupTeardownIncluder {
         updatePageContent();
     }
 
+    private void includePageContent() throws Exception {
+        if (isSuite)
+            includeSuiteSetupPage();
+        includeSetupPage();
+    }
+
 }
