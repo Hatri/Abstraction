b554c63935bf87c25e1124d88314b9236a7b9a75
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-22 18:01:12 +0200

    Abstract, clean code

---
+++ SetupTeardownIncluder.java
@@ -0,0 +1,12 @@
+package fitnesse.html;
+
+import fitnesse.responders.run.SuiteResponder;
+import fitnesse.wiki.*;
+
+public class SetupTeardownIncluder {
+    private PageData pageData;
+    private boolean isSuite;
+    private WikiPage testPage;
+    private StringBuffer newPageContent;
+    private PageCrawler pageCrawler;
+}
