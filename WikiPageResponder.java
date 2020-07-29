5f26ee432a83e92a6fe6d740a37634beadd288a1
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-29 14:26:00 +0200

    Setup

---
+++ WikiPageResponder.java
@@ -0,0 +1,8 @@
+public class WikiPageResponder {
+    protected WikiPage page;
+    protected PageData pageData;
+    protected String pageTitle;
+    protected Request request;
+    protected PageCrawler crawler;
+
+}
