b1b8bae2088a21f92cee8fba2fdbb890f220d1b0
Hatri <18404171+Hatri@users.noreply.github.com>
2020-08-08 01:57:57 +0200

    Update

--- EBJ3Bank.java
+++ EBJ3Bank.java
@@ -9,4 +9,19 @@ public class Bank implements java.io.Serializable {
     @id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @Embeddable
+    public class Address {
+        protected String streetAddr1;
+        protected String streetAddr2;
+        protected String city;
+        protected String state;
+        protected String zipCode;
+    }
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(cascade = CascadeType.All, fetch = FetchType.EAGER, mappedBy = "bank")
+    private Collection<Account> accounts = new ArrayList<Account>();
 }
